aa5bb809805f45db44c9048d81c88c7bccf5ef3b
samirsherlekar <sas450@pitt.edu>
2020-03-19 14:36:59 -0400

    mongodb and mongose get, put, patch, delete

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect("mongodb+srv://sas450db:" + 
+                    process.env.MONGO_ATLAS_PW + 
+                    "@rest-ibewj.mongodb.net/test?retryWrites=true&w=majority", 
+                {   
+                    useNewUrlParser: true , 
+                    useUnifiedTopology : true
+                }).catch(error => console.log(error));
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+//Handling Cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+  
 
 //Routes which should handle requests
 app.use('/products', productRoutes);
